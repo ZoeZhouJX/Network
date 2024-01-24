8e631a9bb93422d3bb190bc301bbe38a4c3fa21f
zjx <zjx@163.com>
2024-01-24 15:49:30 +0800

    [修改] 备份读集合

--- multiIO/select/demoSelectServer.c
+++ multiIO/select/demoSelectServer.c
@@ -5,18 +5,18 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <strings.h>
-#include <sys/time.h>
 #include <sys/select.h>
+#include <sys/time.h>
 #include <unistd.h>
 #include <time.h>
 #include <ctype.h>
 
 #define SERVER_PORT 8080
 #define MAX_LISTEN 128
+
 #define BUFFER_SIZE 128
 
-/* 用单进程/线程实现并发 */
-
+/* 用单进程/线程 实现并发 */
 
 int main()
 {
@@ -27,6 +27,7 @@ int main()
         perror("socket error");
         exit(-1);
     }
+    printf("sockfd:%d\n", sockfd);
 
     /* 将本地的IP和端口绑定 */
     struct sockaddr_in localAddress;
@@ -36,7 +37,7 @@ int main()
     localAddress.sin_addr.s_addr = htonl(INADDR_ANY);
 
     socklen_t localAddressLen = sizeof(localAddress);
-    int ret = bind(sockfd, (struct sockaddr *)&localAddress,localAddressLen);
+    int ret = bind(sockfd, (struct sockaddr *)&localAddress, localAddressLen);
     if (ret == -1)
     {
         perror("bind error");
@@ -44,34 +45,37 @@ int main()
     }
 
     /* 监听 */
+    ret = listen(sockfd, MAX_LISTEN);
+    if (ret == -1)
     {
-        ret = listen(sockfd, MAX_LISTEN);
-        if (ret == -1)
-        {
-            perror("listen error");
-            exit(-1);
-        }
+        perror("listen error");
+        exit(-1);
     }
 
     fd_set readSet;
     /* 清空集合 */
     FD_ZERO(&readSet);
-    /* 把监听的文件描述符添加到读集合中，让内核帮忙检测 */
+    /* 把监听的文件描述符添加到读集合中, 让内核帮忙检测 */
     FD_SET(sockfd, &readSet);
 
     int maxfd = sockfd;
 
-    #if 0
+#if 0
     /* 设置超时 */
     struct timeval timeValue;
     bzero(&timeValue, sizeof(timeValue));
     timeValue.tv_sec = 5;
     timeValue.tv_usec = 0;
-    #endif
+#endif
 
+    fd_set tmpReadSet;
+    /* 清除脏数据 */
+    bzero(&tmpReadSet, sizeof(tmpReadSet));
     while (1)
     {
-        ret = select(maxfd + 1, &readSet, NULL, NULL, NULL);
+        /* 备份读集合 */
+        tmpReadSet = readSet;
+        ret = select(maxfd + 1, &tmpReadSet, NULL, NULL, NULL);
         if (ret == -1)
         {
             perror("select error");
@@ -79,30 +83,30 @@ int main()
         }
 
         /* 如果sockfd在readSet集合里面 */
-        if (FD_ISSET(sockfd, &readSet))
+        if (FD_ISSET(sockfd, &tmpReadSet))
         {
             int acceptfd = accept(sockfd, NULL, NULL);
             if (acceptfd == -1)
             {
-                perror("accept error");
+                perror("accpet error");
                 break;
             }
-            /* 将通信的句柄放到读集合 */
+            /* 将通信的句柄 放到读集合 */
             FD_SET(acceptfd, &readSet);
 
             /* 更新maxfd的值 */
             maxfd = maxfd < acceptfd ? acceptfd : maxfd;
         }
 
-        /* 程序到这个地方：说明可能有通信 */
+        /* 程序到这个地方: 说明可能有通信 */
         for (int idx = 0; idx <= maxfd; idx++)
         {
-            if (idx != sockfd && FD_ISSET(idx, &readSet))
+            if (idx != sockfd && FD_ISSET(idx, &tmpReadSet))
             {
                 char buffer[BUFFER_SIZE];
                 /* 清除脏数据 */
                 bzero(buffer, sizeof(buffer));
-                /* 程序到这里，一定有通信（老客户）*/
+                /* 程序到这里, 一定有通信(老客户) */
                 int readBytes = read(idx, buffer, sizeof(buffer) - 1);
                 if (readBytes < 0)
                 {
@@ -140,6 +144,7 @@ int main()
             }
         }
     }
+
     /* 关闭文件描述符 */
     close(sockfd);
 
